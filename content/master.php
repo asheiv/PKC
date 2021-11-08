acce5257e71fcd5f9d9cab1ff927aa2e6099e5b3
asheiv <92508904+asheiv@users.noreply.github.com>
2021-11-08 09:27:25 +0700

    Update master.php

--- content/master.php
+++ content/master.php
@@ -1,25 +1,15 @@
 <?php 
-  error_reporting(0);
-  $page =$_GET['p'];
-  switch($page)
-  {
-   default:
-    include 'home.php';
-    break;
-    case "home";
-      include 'home.php';
-    break;
-    case "email";
-      include 'email.php';
-    break;
-	case "no_hp";
-      include 'no_hp.php';
-    break;
-	case "map";
-      include 'map.php';
-    break;
-	case "portofolio";
-      include 'portofolio.php';
-    break;
-  }
+if(empty($_GET['page'])) {
+	require_once 'content/home.php';
+}elseif ($_GET['page']=='home') {
+	require_once 'content/home.php';
+}elseif ($_GET['page']=='email') {
+	require_once 'content/email.php';
+}elseif ($_GET['page']=='kontak') {
+	require_once 'content/kontak.php';
+}elseif ($_GET['page']=='map') {
+	require_once 'content/map.php';
+}elseif ($_GET['page']=='portofolio') {
+	require_once 'content/portofolio.php';
+}
 ?>
