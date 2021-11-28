d47c61edcc782c4bae01ff46fbd2bbe8aa8c7f34
asheiv <92508904+asheiv@users.noreply.github.com>
2021-11-28 17:22:47 +0700

    Update home.php

--- content/home.php
+++ content/home.php
@@ -6,7 +6,7 @@
 
 
 
-<center><img src="content\logo.png" width="400" height="200"></center>
+<center><img src="content\logo_auto_x2.jpg" width="400" height="200"></center>
 <h1>PT. Pramadarya Karya Cipta</h1>
 <p><h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;PT. Pramadaya Karya Cipta didirikan pada tahun 2008, sebuah perusahaan yang bergerak di bidang konsultan dan jasa. 
 Perusahaan kami beranggotakan tenaga ahli yang sangat berkompeten di bidangnya, baik konstruksi maupun non konstruksi. 
