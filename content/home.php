cbffcfb10a1575ee10a0643bfba6545a8a789e71
asheiv <92508904+asheiv@users.noreply.github.com>
2021-11-30 10:06:59 +0700

    Update home.php

--- content/home.php
+++ content/home.php
@@ -1,4 +1,3 @@
-
 <link href="\css\main.css" rel="stylesheet">
 <title>W</title>
 <body>
@@ -6,7 +5,7 @@
 
 
 
-<center><img src="content\logo_auto_x2.jpg" width="400" height="200"></center>
+<center><img src="image\logo_auto_x2.jpg" width="400" height="200"></center>
 <h1>PT. Pramadarya Karya Cipta</h1>
 <p><h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;PT. Pramadaya Karya Cipta didirikan pada tahun 2008, sebuah perusahaan yang bergerak di bidang konsultan dan jasa. 
 Perusahaan kami beranggotakan tenaga ahli yang sangat berkompeten di bidangnya, baik konstruksi maupun non konstruksi. 
