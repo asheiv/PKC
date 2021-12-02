1c577f6d62fcfcbae1e8afb2cfd1d519556eee83
asheiv <92508904+asheiv@users.noreply.github.com>
2021-12-02 10:38:03 +0700

    Update master.php

--- content/master.php
+++ content/master.php
@@ -9,7 +9,13 @@ if(empty($_GET['page'])) {
 	require_once 'content/kontak.php';
 }elseif ($_GET['page']=='map') {
 	require_once 'content/map.php';
-}elseif ($_GET['page']=='portofolio') {
-	require_once 'content/portofolio.php';
+}elseif ($_GET['page']=='2017') {
+	require_once 'content/2017.php';
+}elseif ($_GET['page']=='2018') {
+	require_once 'content/2018.php';
+}elseif ($_GET['page']=='2019') {
+	require_once 'content/2019.php';
+}elseif ($_GET['page']=='2020') {
+	require_once 'content/2020.php';
 }
 ?>
