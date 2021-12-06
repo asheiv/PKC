ead89b7fcc494891fa94f4aef55b5af562da9d0c
asheiv <92508904+asheiv@users.noreply.github.com>
2021-12-06 08:22:06 +0700

    Add files via upload

---
+++ content/konstruksi.php
@@ -0,0 +1,39 @@
+
+
+<h1 style="text-align:center">Civil and Structure Engineering</h1>
+<hr>
+<table>
+
+<tr>
+<td><h3>High Rise Buildings</h3></td>
+</tr>
+<tr>
+<td><h3><li>Apartments/Rumah Susun</li></h3></td>
+<td><h3><li>Hotels</li></h3></td>
+<td><h3><li>Offices</li></h3></td>
+<td><h3><li>Departement Stores/Malls</li></h3></td>
+</tr>
+<tr><td colspan="4"><hr></td></tr>
+<tr>
+<td><h3>Bridges</h3></td>
+</tr>
+<tr>
+<td><h3><li>Steel Girder Bridges</li></h3></td>
+<td><h3><li>Steel Box Girder Bridges/Flyover</li></h3></td>
+<td><h3><li>Warren Truss Bridges</li></h3></td>
+</tr>
+<tr>
+<td><h3><li>Suspension Bridges</li></h3></td>
+<td><h3><li>Cable Stayed Bridges</li></h3></td>
+</tr>
+<tr><td colspan="4"><hr></td></tr>
+<tr>
+<td><h3>Towers/Poles</h3></td>
+</tr>
+<tr>
+<td><h3><li>Telecommunication Towers/Poles Cellullar /rural, GSM, CDMA</li></h3></td>
+<td><h3><li>Transmission Towers/Poles PLN 70kV, 150 kV, 275 kV and 500 kV</li></h3></td>
+<td><h3><li>Highmast</li></h3></td>
+</tr>
+
+</table>
---
+++ content/non_konstruksi.php
@@ -0,0 +1,74 @@
+
+
+<h1 style="text-align:center">Perizinan</h1>
+<hr>
+<table>
+
+<tr>
+<td><h3><li>Mendirikan Badan Usaha (CV,PD,PT,Koperasi,dll)</li></h3></td>
+<td><h3><li>Izin Gangguan (IG/HO)</li></h3></td>
+<td><h3><li>Tanda Daftar Perusahaan (TDP)</li></h3></td>
+</tr>
+<tr>
+<td><h3><li>Tanda Daftar Gudang (TDG)</li></h3></td>
+<td><h3><li>Tanda Daftar Industri (TDI)</li></h3></td>
+<td><h3><li>Kerja Izin Usaha Perdagangan (IUP/SIUP)</li></h3></td>
+</tr>
+<tr>
+<td><h3><li>Izin Usaha Industri (IUI)</li></h3></td>
+<td><h3><li>Izin Usaha Jasa Konstruksi (IUJK)</li></h3></td>
+<td><h3><li>Izin Mendirikan Bangunan (IMB)</li></h3></td>
+</tr>
+<tr>
+<td><h3><li>Izin Mendirikan Banungan Menara Komunikasi</li></h3></td>
+<td><h3><li>Izin Lokasi</li></h3></td>
+<td><h3><li>Izin Penggalian Ruang Milik Jalan (RUMIJA)</li></h3></td>
+</tr>
+<tr>
+<td><h3><li>Izin Pembuatan Jalan Masuk Pekarangan</li></h3></td>
+<td><h3><li>Izin Penutupan/Penggunaan Trotoar, Berm dan Saluran</li></h3></td>
+<td><h3><li>Izin Pematangan Lahan/Tanah</li></h3></td>
+</tr>
+<tr>
+<td><h3><li>Izin Pembuatan Jalan Masuk di Dalam Kompleks Perumahan, Pertokoan dan yang sejenisnya</li></h3></td>
+<td><h3><li>Izin Pemanfaatan Titik Tiang Pancang Reklame, Jembatan Penyeberangan Orang (JPO) dan Sejenisnya</li></h3></td>
+<td><h3><li>Izin Reklame</li></h3></td>
+</tr>
+<tr>
+<td><h3><li>Izin Penyelenggaraan Angkutan Kota</li></h3></td>
+<td><h3><li>Izin Pengelolaan Tempat Parkir (IPTP)</li></h3></td>
+<td><h3><li>Izin Usaha Angkutan (IUA)</li></h3></td>
+</tr>
+<tr>
+<td><h3><li>Izin Jasa Titipan</li></h3></td>
+<td><h3><li>Izin Pembuangan Air Buangan ke Sumber Air</li></h3></td>
+<td><h3><li>Izin Pengelolaan Air Bawah Tanah</li></h3></td>
+</tr>
+<tr>
+<td><h3><li>Izin Perubahan Alur, Bentuk, Dimensi dan Kemiringan Dasar Saluran/Sungai</li></h3></td>
+<td><h3><li>Izin Pembangunan Lintasan yang Berada dibawahnya/Diatasnya</li></h3></td>
+<td><h3><li>Izin Pemanfaat Bangunan Pengairan dan Lahan Pada Daerah Sempadan Saluran Sungai</li></h3></td>
+</tr>
+<tr>
+<td><h3><li>Izin Pemanfaatan Lahan Mata Air dan Lahan Pengairan Lainnya</li></h3></td>
+</tr>
+</table>
+<hr>
+<h1 style="text-align:center">Manajemen Pajak</h1>
+<hr>
+<table>
+<tr>
+<td><h3><li>Internal Audit (Bekerja sama dengan kantor akuntan publik)</li></h3></td>
+<td><h3><li>Internal Control</li></h3></td>
+<td><h3><li>Jasa Pembuatan Laporan Keuangan</li></h3></td>
+</tr>
+<tr>
+<td><h3><li>Jasa Pembukuan</li></h3></td>
+<td><h3><li>Pembuatan SOP</li></h3></td>
+<td><h3><li>Management ISO</li></h3></td>
+</tr>
+<tr>
+<td><h3><li>Pembuatan SPT & Konsultasi Pajak</li></h3></td>
+<td><h3><li>Jasa Pengamanan Keamanan (bekerjasama dengan angkatan)</li></h3></td>
+</tr>
+</table>
