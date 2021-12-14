f4b0954b882b98ffb3d62810b6915589d1b0c13b
asheiv <92508904+asheiv@users.noreply.github.com>
2021-12-14 10:12:59 +0700

    Update non_konstruksi.php

--- content/non_konstruksi.php
+++ content/non_konstruksi.php
@@ -54,21 +54,3 @@
 </tr>
 </table>
 <hr>
-<h1 style="text-align:center">Manajemen Pajak</h1>
-<hr>
-<table>
-<tr>
-<td><h3><li>Internal Audit (Bekerja sama dengan kantor akuntan publik)</li></h3></td>
-<td><h3><li>Internal Control</li></h3></td>
-<td><h3><li>Jasa Pembuatan Laporan Keuangan</li></h3></td>
-</tr>
-<tr>
-<td><h3><li>Jasa Pembukuan</li></h3></td>
-<td><h3><li>Pembuatan SOP</li></h3></td>
-<td><h3><li>Management ISO</li></h3></td>
-</tr>
-<tr>
-<td><h3><li>Pembuatan SPT & Konsultasi Pajak</li></h3></td>
-<td><h3><li>Jasa Pengamanan Keamanan (bekerjasama dengan angkatan)</li></h3></td>
-</tr>
-</table>
