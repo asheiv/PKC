58686356a5043d295bc6d5e9b7b1ad00118d8e0f
asheiv <92508904+asheiv@users.noreply.github.com>
2021-12-14 10:11:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,28 +9,29 @@
 
 <ul>
 
-<li><a href="home"><b>Home</b></a></li>
+<li><a href="Home"><b>Home</b></a></li>
 <li class="dropdown">
     <a href="#" class="dropbtn"><b>Jasa</b></a>
     <div class="dropdown-content">
-      <a href="non_konstruksi"><b>Non-Konstruksi</a>
-      <a href="konstruksi">Konstruksi</a>
+      <a href="Perizinan"><b>Perizinan</b></a>
+      <a href="Manajemen"><b>Manajemen</b></a>
+      <a href="Konstruksi"><b>Konstruksi<b/></a>
 
     </div>
   </li>
 <li class="dropdown">
     <a href="#" class="dropbtn">Kontak</a>
     <div class="dropdown-content">
-      <a href="email"><b>E-Mail</a>
-      <a href="kontak">No. HP</a>
-      <a href="map">Maps</b></a>
+      <a href="Email"><b>E-Mail</a>
+      <a href="No-Telp">No. HP</a>
+      <a href="Map">Maps</b></a>
     </div>
   </li>
 <li class="dropdown">
     <a href="#" class="dropbtn">Portofolio</a>
     <div class="dropdown-content">
       <a href="2017"><b>2017</a>
-	  <a href="2018">2018</a>
+      <a href="2018">2018</a>
       <a href="2019">2019</a>
       <a href="2020">2020</b></a>
     </div>
