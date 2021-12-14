f01bf690578c183f2c180f731c657f63250c7266
asheiv <92508904+asheiv@users.noreply.github.com>
2021-12-14 10:11:47 +0700

    Update master.php

--- content/master.php
+++ content/master.php
@@ -1,25 +1,41 @@
 <?php 
-if(empty($_GET['page'])) {
-	require_once 'content/home.php';
-}elseif ($_GET['page']=='home') {
-	require_once 'content/home.php';
-}elseif ($_GET['page']=='email') {
-	require_once 'content/email.php';
-}elseif ($_GET['page']=='kontak') {
-	require_once 'content/kontak.php';
-}elseif ($_GET['page']=='map') {
-	require_once 'content/map.php';
-}elseif ($_GET['page']=='2017') {
-	require_once 'content/2017.php';
-}elseif ($_GET['page']=='2018') {
-	require_once 'content/2018.php';
-}elseif ($_GET['page']=='2019') {
-	require_once 'content/2019.php';
-}elseif ($_GET['page']=='2020') {
-	require_once 'content/2020.php';
-}elseif ($_GET['page']=='non_konstruksi') {
-	require_once 'content/non_konstruksi.php';
-}elseif ($_GET['page']=='konstruksi') {
-	require_once 'content/konstruksi.php';
+$page = $_GET['page'];
+switch ($page) {
+case 'Home':
+    require_once('content/home.php');
+    break;
+case 'Email':
+    require_once('content/email.php');
+    break;
+case 'No-Telp':
+    require_once('content/no_telp.php');
+    break;
+case 'Map':
+    require_once('content/map.php');
+    break;
+case '2017':
+    require_once('content/2017.php');
+    break;
+case '2018':
+    require_once('content/2018.php');
+    break;
+case '2019':
+    require_once('content/2019.php');
+    break;
+case '2020':
+    require_once('content/2020.php');
+    break;
+case 'Konstruksi':
+    require_once('content/konstruksi.php');
+    break;
+case 'Perizinan':
+    require_once('content/perizinan.php');
+    break;
+case 'Manajemen':
+    require_once('content/manajemen.php');
+    break;	
+  default:
+    require_once('content/home.php');
 }
+
 ?>
