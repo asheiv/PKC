9085b8aaa3a58476717c44c1b71c8f47a4b7dc83
asheiv <92508904+asheiv@users.noreply.github.com>
2021-12-15 09:18:23 +0700

    Update 2017.php

--- content/2017.php
+++ content/2017.php
@@ -1,12 +1,7 @@
-
-
 <h1 style="text-align:center">2017</h1>
 <table>
 <hr>
 <tr><td><h3>PT ALITA PRAYA MITRA</h3></td></tr>
-<tr><td><h3>Deskripsi : </h3></td></tr>
-<tr><td><h3><li>Civil Design & Analyze the Tower Utiliziltion / Capacity with Strengthening </li></h3></td></tr>
-<tr><td><h3><li>Civil Design & Analyze the Tower Utiliziation / Capacity without Strengthening  </li></h3></td></tr>
-
-
-
+<tr><td><h3 style="font-weight:normal;font-size:20px;">Deskripsi : </h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Civil Design & Analyze the Tower Utiliziltion / Capacity with Strengthening </li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Civil Design & Analyze the Tower Utiliziation / Capacity without Strengthening  </li></h3></td></tr>
