ca890ea8dca35610f43dc97294ecd657e3094e61
asheiv <92508904+asheiv@users.noreply.github.com>
2021-12-15 10:33:06 +0700

    Update konstruksi.php

--- content/konstruksi.php
+++ content/konstruksi.php
@@ -1,36 +1,13 @@
-<h1 style="text-align:center">Civil and Structure Engineering</h1>
+<h1 style="text-align:center">Konstruksi</h1>
 <hr>
 <table>
-<tr>
-<td><h3>High Rise Buildings</h3></td>
-</tr>
-<tr>
-<td><h3 style="font-weight:normal;font-size:20px;"><li>Apartments/Rumah Susun</li></h3></td>
-<td><h3 style="font-weight:normal;font-size:20px;"><li>Hotels</li></h3></td>
-<td><h3 style="font-weight:normal;font-size:20px;"><li>Offices</li></h3></td></tr>
-<tr>
-<td><h3 style="font-weight:normal;font-size:20px;"><li>Departement Stores/Malls</li></h3></td></tr>
-</tr>
+<tr><td><h3>Perencanaan Rekayasa</h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Jasa Nasehat dan Konsultasi Rekayas Teknik </li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Jasa Desain Rekayasa untuk Konstruksi Pondasi serta Struktur Bangunan</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Jasa Desain Rekayasa untuk Pekerjaan Teknik Sipil Transportasi</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Jasa Desain Rekayasa Lainnya</li></h3></td></tr></tr>
 <tr><td colspan="4"><hr></td></tr>
-<tr>
-<td><h3>Bridges</h3></td>
-</tr>
-<tr>
-<td><h3 style="font-weight:normal;font-size:20px;"><li>Steel Girder Bridges</li></h3></td>
-<td><h3 style="font-weight:normal;font-size:20px;"><li>Steel Box Girder Bridges/Flyover</li></h3></td>
-<td><h3 style="font-weight:normal;font-size:20px;"><li>Warren Truss Bridges</li></h3></td>
-</tr>
-<tr>
-<td><h3 style="font-weight:normal;font-size:20px;"><li>Suspension Bridges</li></h3></td>
-<td><h3 style="font-weight:normal;font-size:20px;"><li>Cable Stayed Bridges</li></h3></td>
-</tr>
-<tr><td colspan="4"><hr></td></tr>
-<tr>
-<td><h3 ">Towers/Poles</h3></td>
-</tr>
-<tr>
-<td><h3 style="font-weight:normal;font-size:20px;"><li>Telecommunication Towers/Poles Cellullar /rural, GSM, CDMA</li></h3></td>
-<td><h3 style="font-weight:normal;font-size:20px;"><li>Transmission Towers/Poles PLN 70kV, 150 kV, 275 kV and 500 kV</li></h3></td>
-<td><h3 style="font-weight:normal;font-size:20px;"><li>Highmast</li></h3></td>
-</tr>
+<tr><td><h3>Pengawasan Rekayasa</h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Jasa Pengawas Konstruksi Bangunan Gedung</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Jasa Pengawas Pekerjaan Konstruksi Teknik Sipil Transportasi</li></h3></td></tr>
 </table>
