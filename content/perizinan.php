5b4316ce10ac41d88aa957f51df4e48bbf8881d0
asheiv <92508904+asheiv@users.noreply.github.com>
2021-12-15 10:32:22 +0700

    Update perizinan.php

--- content/perizinan.php
+++ content/perizinan.php
@@ -1,53 +1,5 @@
 <h1 style="text-align:center">Perizinan</h1>
-<hr>
 <table>
-<tr>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Mendirikan Badan Usaha (CV,PD,PT,Koperasi,dll)</li></h3></td>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Izin Gangguan (IG/HO)</li></h3></td>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Tanda Daftar Perusahaan (TDP)</li></h3></td>
-</tr>
-<tr>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Tanda Daftar Gudang (TDG)</li></h3></td>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Tanda Daftar Industri (TDI)</li></h3></td>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Kerja Izin Usaha Perdagangan (IUP/SIUP)</li></h3></td>
-</tr>
-<tr>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Izin Usaha Industri (IUI)</li></h3></td>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Izin Usaha Jasa Konstruksi (IUJK)</li></h3></td>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Izin Mendirikan Bangunan (IMB)</li></h3></td>
-</tr>
-<tr>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Izin Mendirikan Banungan Menara Komunikasi</li></h3></td>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Izin Lokasi</li></h3></td>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Izin Penggalian Ruang Milik Jalan (RUMIJA)</li></h3></td>
-</tr>
-<tr>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Izin Pembuatan Jalan Masuk Pekarangan</li></h3></td>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Izin Penutupan/Penggunaan Trotoar, Berm dan Saluran</li></h3></td>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Izin Pematangan Lahan/Tanah</li></h3></td>
-</tr>
-<tr>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Izin Pembuatan Jalan Masuk di Dalam Kompleks Perumahan, Pertokoan dan yang sejenisnya</li></h3></td>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Izin Pemanfaatan Titik Tiang Pancang Reklame, Jembatan Penyeberangan Orang (JPO) dan Sejenisnya</li></h3></td>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Izin Reklame</li></h3></td>
-</tr>
-<tr>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Izin Penyelenggaraan Angkutan Kota</li></h3></td>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Izin Pengelolaan Tempat Parkir (IPTP)</li></h3></td>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Izin Usaha Angkutan (IUA)</li></h3></td>
-</tr>
-<tr>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Izin Jasa Titipan</li></h3></td>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Izin Pembuangan Air Buangan ke Sumber Air</li></h3></td>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Izin Pengelolaan Air Bawah Tanah</li></h3></td>
-</tr>
-<tr>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Izin Perubahan Alur, Bentuk, Dimensi dan Kemiringan Dasar Saluran/Sungai</li></h3></td>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Izin Pembangunan Lintasan yang Berada dibawahnya/Diatasnya</li></h3></td>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Izin Pemanfaat Bangunan Pengairan dan Lahan Pada Daerah Sempadan Saluran Sungai</li></h3></td>
-</tr>
-<tr>
-<td><h3 style="font-weight:normal;font-size:20px;" ><li>Izin Pemanfaatan Lahan Mata Air dan Lahan Pengairan Lainnya</li></h3></td>
-</tr>
-</table>
+<tr><td><h3 style="font-weight:normal;font-size:20px;" ><li>Surat Izin Usaha Perdagangan (SIUP)</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;" ><li>Izin Usaha Jasa Konstruksi</li></h3></td></tr>
 <hr>
