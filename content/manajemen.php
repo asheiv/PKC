651e251fc5d0d69bc6fc77ee6b8601a0471cb123
asheiv <92508904+asheiv@users.noreply.github.com>
2021-12-15 09:23:24 +0700

    Update manajemen.php

--- content/manajemen.php
+++ content/manajemen.php
@@ -2,18 +2,18 @@
 <hr>
 <table>
 <tr>
-<td><h3><li>Internal Audit (Bekerja sama dengan kantor akuntan publik)</li></h3></td>
-<td><h3><li>Internal Control</li></h3></td>
-<td><h3><li>Jasa Pembuatan Laporan Keuangan</li></h3></td>
+<td><h3 style="font-weight:normal;font-size:20px;"><li>Internal Audit (Bekerja sama dengan kantor akuntan publik)</li></h3></td>
+<td><h3 style="font-weight:normal;font-size:20px;"><li>Internal Control</li></h3></td>
+<td><h3 style="font-weight:normal;font-size:20px;"><li>Jasa Pembuatan Laporan Keuangan</li></h3></td>
 </tr>
 <tr>
-<td><h3><li>Jasa Pembukuan</li></h3></td>
-<td><h3><li>Pembuatan SOP</li></h3></td>
-<td><h3><li>Management ISO</li></h3></td>
+<td><h3 style="font-weight:normal;font-size:20px;"><li>Jasa Pembukuan</li></h3></td>
+<td><h3 style="font-weight:normal;font-size:20px;"><li>Pembuatan SOP</li></h3></td>
+<td><h3 style="font-weight:normal;font-size:20px;"><li>Management ISO</li></h3></td>
 </tr>
 <tr>
-<td><h3><li>Pembuatan SPT & Konsultasi Pajak</li></h3></td>
-<td><h3><li>Jasa Pengamanan Keamanan (bekerjasama dengan angkatan)</li></h3></td>
+<td><h3 style="font-weight:normal;font-size:20px;"><li>Pembuatan SPT & Konsultasi Pajak</li></h3></td>
+<td><h3 style="font-weight:normal;font-size:20px;"><li>Jasa Pengamanan Keamanan (bekerjasama dengan angkatan)</li></h3></td>
 </tr>
 </table>
 <hr>
