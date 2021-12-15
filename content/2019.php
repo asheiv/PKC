6c7979dc4567857cadfdb3e05d5406c3420ea4ad
asheiv <92508904+asheiv@users.noreply.github.com>
2021-12-15 09:20:06 +0700

    Update 2019.php

--- content/2019.php
+++ content/2019.php
@@ -2,45 +2,45 @@
 <table>
 <hr>
 <tr><td><h3>BIT TEKNOLOGI</h3></td></tr>
-<tr><td><h3>Deskripsi : </h3></td></tr>
-<tr><td><h3><li>Analysis Design </li></h3></td></tr>
-<tr><td><h3><li>Drawing Tower Foundation</li></h3></td></tr>
-<tr><td><h3><li>Mockup / POC Tower</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;">Deskripsi : </h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Analysis Design </li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Drawing Tower Foundation</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Mockup / POC Tower</li></h3></td></tr>
 </table>
 <hr>
 <table>
 <tr><td><h3>PT DCT TOTAL SOLUTION</h3></td></tr>
-<tr><td><h3>Deskripsi : </h3></td></tr>
-<tr><td><h3><li>Jasa SBIP dengan Pondasi dan Kerangka Portal Cantilever</li></h3></td></tr>
-<tr><td><h3><li>Hammer Test</li></h3></td></tr>
-<tr><td><h3><li>Jaminan Desain akibat Penurunan Mutu Beton</li></h3></td></tr>
-<tr><td><h3><li>Desain Pondasi</li></h3></td></tr>
-<tr><td><h3><li>Pemerikasaan Desain Struktur</li></h3></td></tr>
-<tr><td><h3><li>Penerbitan SIBP</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;">Deskripsi : </h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Jasa SBIP dengan Pondasi dan Kerangka Portal Cantilever</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Hammer Test</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Jaminan Desain akibat Penurunan Mutu Beton</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Desain Pondasi</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Pemerikasaan Desain Struktur</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Penerbitan SIBP</li></h3></td></tr>
 </table>
 <hr>
 <table>
 <tr><td><h3>PT INDOSAT TBK</h3></td></tr>
-<tr><td><h3>Deskripsi : </h3></td></tr>
-<tr><td><h3><li>Consultant Tracking Fee</li></h3></td></tr>
-<tr><td><h3><li>Site Audit</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;">Deskripsi : </h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Consultant Tracking Fee</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Site Audit</li></h3></td></tr>
 </table>
 <hr>
 <table>
 <tr><td><h3>PT MATAWARI LINTAS NUSA</h3></td></tr>
-<tr><td><h3>Deskripsi : </h3></td></tr>
-<tr><td><h3><li>Survey Tower (No-Strengthening)</li></h3></td></tr>
-<tr><td><h3><li>Survey Tower (Strenghthening)</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;">Deskripsi : </h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Survey Tower (No-Strengthening)</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Survey Tower (Strenghthening)</li></h3></td></tr>
 </table>
 <hr>
 <table>
 <tr><td><h3>PT ALITA PRAYA MITRA</h3></td></tr>
-<tr><td><h3>Deskripsi : </h3></td></tr>
-<tr><td><h3><li>Civil Design & Analyze the Tower Utiliziation / Capacity without Strengthening  </li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;">Deskripsi : </h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Civil Design & Analyze the Tower Utiliziation / Capacity without Strengthening  </li></h3></td></tr>
 </table>
 <hr>
 <table>
 <tr><td><h3>PT TWINK INDONESIA</h3></td></tr>
-<tr><td><h3>Deskripsi : </h3></td></tr>
-<tr><td><h3><li>Jasa Desain Gambar Sipil  </li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;">Deskripsi : </h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Jasa Desain Gambar Sipil  </li></h3></td></tr>
 </table>
