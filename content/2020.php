1446383ac5e655dd43564fe202bb96475e6d46da
asheiv <92508904+asheiv@users.noreply.github.com>
2021-12-15 09:20:43 +0700

    Update 2020.php

--- content/2020.php
+++ content/2020.php
@@ -2,39 +2,39 @@
 <hr>
 <table>
 <tr><td><h3>PT ALITA PRAYA MITRA</h3></td></tr>
-<tr><td><h3>Deskripsi : </h3></td></tr>
-<tr><td><h3><li>Civil Design & Analyze the Tower Utiliziltion / Capacity with Strengthening </li></h3></td></tr>
-<tr><td><h3><li>Civil Design & Analyze the Tower Utiliziation / Capacity without Strengthening  </li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;">Deskripsi : </h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Civil Design & Analyze the Tower Utiliziltion / Capacity with Strengthening </li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Civil Design & Analyze the Tower Utiliziation / Capacity without Strengthening  </li></h3></td></tr>
 </table>
 <hr>
 <table>
 <tr><td><h3>PT BLUESCOPE BUILDINGS INDONESIA</h3></td></tr>
-<tr><td><h3>Deskripsi : </h3></td></tr>
-<tr><td><h3><li>Engineering Drawing for Central Java Project</li></h3></td></tr>
-<tr><td><h3><li>Detail Engineering Design “Coal Terminal“ “Central Java Project – Ammonia Removal System, Ammonia Chemical Shed Area"</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;">Deskripsi : </h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Engineering Drawing for Central Java Project</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Detail Engineering Design “Coal Terminal“ “Central Java Project – Ammonia Removal System, Ammonia Chemical Shed Area"</li></h3></td></tr>
 </table>
 <hr>
 <table>
 <tr><td><h3>PT GLOBAL INDONESIA KOMUNIKATAMA</h3></td></tr>
-<tr><td><h3>Deskripsi : </h3></td></tr>
-<tr><td><h3><li>Strengthening Pole 12m with Drawing</li></h3></td></tr>
-<tr><td><h3><li>Analyze Pole 12m Optimize 2 Operator to 3 Operator</li></h3></td></tr>
-<tr><td><h3><li>Analyze Tower Minaret</li></h3></td></tr>
-<tr><td><h3><li>Strengthening Minaret</li></h3></td></tr>
-<tr><td><h3><li>Analyze Existing Building Due to Additional Operator</li></h3></td></tr>
-<tr><td><h3><li>Analyze Tower SST 40m Optimize 3 Operator to 4 Operator</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;">Deskripsi : </h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Strengthening Pole 12m with Drawing</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Analyze Pole 12m Optimize 2 Operator to 3 Operator</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Analyze Tower Minaret</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Strengthening Minaret</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Analyze Existing Building Due to Additional Operator</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Analyze Tower SST 40m Optimize 3 Operator to 4 Operator</li></h3></td></tr>
 </table>
 <hr>
 <table>
 <tr><td><h3>PT INDOSAT TBK</h3></td></tr>
-<tr><td><h3>Deskripsi : </h3></td></tr>
-<tr><td><h3><li>Site Audit</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;">Deskripsi : </h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Site Audit</li></h3></td></tr>
 </table>
 <hr>
 <table>
 <tr><td><h3>PT MATAWARI LINTAS NUSA</h3></td></tr>
-<tr><td><h3>Deskripsi : </h3></td></tr>
-<tr><td><h3><li>Survey Tower (No-Strengthening)  </li></h3></td></tr>
-<tr><td><h3><li>Survey Tower (Survey/Summary/Report Only)  </li></h3></td></tr>
-<tr><td><h3><li>Sunrey Tower (No-Strengthening & Add Strengthening)  </li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;">Deskripsi : </h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Survey Tower (No-Strengthening)  </li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Survey Tower (Survey/Summary/Report Only)  </li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Sunrey Tower (No-Strengthening & Add Strengthening)  </li></h3></td></tr>
 </table>
