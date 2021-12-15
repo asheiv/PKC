ef890f636067e34175d05cd02594a685e80f6660
asheiv <92508904+asheiv@users.noreply.github.com>
2021-12-15 09:19:19 +0700

    Update 2018.php

--- content/2018.php
+++ content/2018.php
@@ -2,38 +2,36 @@
 <table>
 <hr>
 <tr><td><h3>PT ALITA PRAYA MITRA</h3></td></tr>
-<tr><td><h3>Deskripsi : </h3></td></tr>
-<tr><td><h3><li>Civil Design & Analyze the Tower Utiliziation / Capacity without Strengthening  </li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;">Deskripsi : </h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Civil Design & Analyze the Tower Utiliziation / Capacity without Strengthening  </li></h3></td></tr>
 </table>
 <hr>
 <table>
 <tr><td><h3>PT BUDI BAKTI PRIMA</h3></td></tr>
-<tr><td><h3>Deskripsi : </h3></td></tr>
-<tr><td><h3><li>DED Coal Terminal & Coal Handling Facilities Pulau Gading </li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;">Deskripsi : </h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>DED Coal Terminal & Coal Handling Facilities Pulau Gading </li></h3></td></tr>
 </table>
 <table>
 <tr><td><h3>PT GLOBAL INDONESIA KOMUNIKATAMA</h3></td></tr>
-<tr><td><h3>Deskripsi : </h3></td></tr>
-<tr><td><h3><li>Soil Test </li></h3></td></tr>
-<tr><td><h3><li>Analysis Design Foundation of Perumahan Grand Gading </li></h3></td></tr>
-<tr><td><h3><li>Analysis Design Foundation of Ayani 4 </li></h3></td></tr>
-<tr><td><h3><li>Analysis Design Foundation of Sitabusira </li></h3></td></tr>
-<tr><td><h3><li>Transportation and Mobilization of Tools and Manpower </li></h3></td></tr>
-<tr><td><h3><li>Design Review Stub Leg Existing</li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;">Deskripsi : </h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Soil Test </li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Analysis Design Foundation of Perumahan Grand Gading </li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Analysis Design Foundation of Ayani 4 </li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Analysis Design Foundation of Sitabusira </li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Transportation and Mobilization of Tools and Manpower </li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Design Review Stub Leg Existing</li></h3></td></tr>
 </table>
 <hr>
 <table>
 <tr><td><h3>PT MATAWARI LINTAS NUSA</h3></td></tr>
-<tr><td><h3>Deskripsi : </h3></td></tr>
-<tr><td><h3><li>Suryey Tower (No-Strengthening) </li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;">Deskripsi : </h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Suryey Tower (No-Strengthening) </li></h3></td></tr>
 </table>
 <hr>
 <table>
 <tr><td><h3>PT PEMBANGUNAN PERUMAHAN (PERSERO) TBK</h3></td></tr>
-<tr><td><h3>Deskripsi : </h3></td></tr>
-<tr><td><h3><li>Detail Engineering Design </li></h3></td></tr>
-<tr><td><h3><li>Tahap Gambar Kerja & Penyusunan BoQ </li></h3></td></tr>
-<tr><td><h3><li>Pengawasan Berkala </li></h3></td></tr>
-
-
+<tr><td><h3 style="font-weight:normal;font-size:20px;">Deskripsi : </h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Detail Engineering Design </li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Tahap Gambar Kerja & Penyusunan BoQ </li></h3></td></tr>
+<tr><td><h3 style="font-weight:normal;font-size:20px;"><li>Pengawasan Berkala </li></h3></td></tr>
 </table>
