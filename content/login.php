7f0192810f2deb510e41aa6419058bc4412ba4c7
asheiv <92508904+asheiv@users.noreply.github.com>
2022-02-09 18:27:07 +0700

    Add files via upload

---
+++ content/login.php
@@ -0,0 +1,10 @@
+<center>
+<div class="loginbox">
+<form action="content/p_login.php" method="post">
+<table style="padding-top:25px;">
+<tr><td>Username</td><td>:</td><td><input type="text" name="username"></td></tr>
+<tr><td>Password</td><td>:</td><td><input type="password" name="password"></td></tr>
+<tr><td colspan="3"><input type="submit" value="Login"></td></tr>
+</form>
+</div>
+</center>
