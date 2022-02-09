d374e18855357153e662fb53e509bf072de205ae
asheiv <92508904+asheiv@users.noreply.github.com>
2022-02-09 18:26:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <link href="css\main.css" rel="stylesheet">
+<link href="css\input.css" rel="stylesheet">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <head>
 <title>Pramadaya Karya Cipta</title>
@@ -6,16 +7,17 @@
 </head>
 <body>
 <div class="container">
+<div class="header-t">Pramadaya Karya Cipta</div>
 <ul>
-<li><a href="Home"><b>Home</b></a></li>
-<li class="dropdown">
+<li><a href="Home" style="float:left"><b>Home</b></a></li>
+<li class="dropdown" style="float:left">
     <a href="#" class="dropbtn"><b>Jasa</b></a>
     <div class="dropdown-content">
       <a href="Konstruksi"><b>Konstruksi</b></a>
       <a href="Perizinan"><b>Perizinan<b/></a>
     </div>
   </li>
-<li class="dropdown">
+<li class="dropdown" style="float:left">
     <a href="#" class="dropbtn">Kontak</a>
     <div class="dropdown-content">
       <a href="Email"><b>E-Mail</a>
@@ -23,7 +25,7 @@
       <a href="Map">Maps</b></a>
     </div>
   </li>
-<li class="dropdown">
+<li class="dropdown" style="float:left">
     <a href="#" class="dropbtn">Portofolio</a>
     <div class="dropdown-content">
       <a href="2017"><b>2017</a>
@@ -32,6 +34,7 @@
       <a href="2020">2020</b></a>
     </div>
   </li>
+  <li style="float:right"><a href="Login" ><b>Karyawan</b></a></li>
 </ul>
 <div class="isi">
 <?php include 'content/master.php';?>
